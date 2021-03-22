18aea2339fae00c3d4924ed608cb7c3dfd19aacb
Ira Bakumenko <ibakumenko997@gmail.com>
2021-03-22 17:39:19 +0300

    get, post: async-await

--- app.js
+++ app.js
@@ -1,36 +1,33 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const fs = require('fs')
+const fs = require('fs').promises;
 
 const app = express()
 const jsonParser = bodyParser.json()
 const filePath = "users.json";
 
-app.get("/api/users", function(req, res){
-
-    fs.readFile(filePath,"utf8", function(err, contents) {
-        if(err) {
-            throw err
-        }
+app.get("/api/users", async function(req, res){
+    try {
+        const contents =  await fs.readFile(filePath, "utf8");
         const users = JSON.parse(contents);
         const data = users.map(item => {
             return {id: item.id, name: item.name, age: item.age}
         })
         res.send(data);
-    });
+    } catch (err) {
+        throw err
+    }
 });
 
-app.post('/api/users', jsonParser, function (req, res) {
-    if (!req.body) return res.sendStatus(400)
-
+app.post('/api/users', jsonParser, async function (req, res) {
+    try {
         const userName = req.body.name
         const userAge = req.body.age
         const create_at = Date.now();
         const user = {name: userName, age: userAge, create_at: create_at}
 
-    fs.readFile(filePath, "utf-8", (err, content) => {
-        if (err) { console.log(err) }
-            const users = JSON.parse(content)
+        const contents = await fs.readFile(filePath, "utf-8")
+            const users = JSON.parse(contents)
             const id = Math.max.apply(
                 Math,
                 users.map(function (o) {
@@ -40,17 +37,12 @@ app.post('/api/users', jsonParser, function (req, res) {
             user.id = id + 1
             users.push(user)
             const data = JSON.stringify(users)
-            console.log(data)
-            fs.writeFile(filePath, data, (err) => {
-                if (err) { console.log(err) };
-                if (data) {
-                    res.send(user)
-                }
-            });
-
-    })
+            await fs.writeFile(filePath, data)
+            res.send(user)
+    } catch (req) {
+        if (!req.body) return res.sendStatus(400)
+    }
 })
-
 
 app.listen(3000, function () {
     console.log('Сервер ожидает подключения...')
