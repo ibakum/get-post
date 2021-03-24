de27fcdd8c0484f17c1c8edbe52e59b04b198662
Ira Bakumenko <ibakumenko997@gmail.com>
2021-03-24 17:15:46 +0300

    database postgresql(sequelize)

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express()
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("users", "postgres", "user", {
+    dialect: "postgres",
+    host: "localhost"
+});
+
 
 const usersRouter = require('./routes');
 
@@ -7,4 +13,7 @@ app.use("/api", usersRouter)
 
 app.listen(3000, function () {
     console.log('Сервер ожидает подключения...')
+    sequelize.sync().then(result=>{
+        console.log('Подключение к базе данных...')
+    }).catch(err=>console.log(err))
 })
--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,38 +1,73 @@
-const fs = require('fs').promises;
-const filePath = 'users.json';
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("users", "postgres", "user", {
+    dialect: "postgres",
+    host: "localhost"
+});
+
+const User = sequelize.define("user", {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    age: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+});
 
 module.exports.getUsers = async function(req, res) {
-    console.log(req)
     try {
-        const contents =  await fs.readFile(filePath, "utf8");
-        const users = JSON.parse(contents);
-        const data = users.map(item => {
-            return {id: item.id, name: item.name, age: item.age}
-        })
+        const data = await User.findAll({raw:true})
         res.send(data);
     } catch (err) {
         throw err
     }
 };
 
-module.exports.createUser = async function (req, res) {
+module.exports.createUser = async function(req, res) {
     try {
-        const {name, age} = req.body
-        const create_at = Date.now();
-        const user = { name, age, create_at }
-
-        const contents = await fs.readFile(filePath, "utf-8")
-        const users = JSON.parse(contents)
-        const id = Math.max.apply(
-            Math,
-            users.map(user => user.id)
-        )
-        user.id = id + 1
-        users.push(user)
-        const data = JSON.stringify(users)
-        await fs.writeFile(filePath, data)
+        console.log(req)
+        const user = await User.create({
+            id: req.query.id,
+            name: req.body.name,
+            age: req.body.age
+        })
         res.send(user)
     } catch (req) {
         if (!req.body) return res.sendStatus(400)
     }
 }
+
+
+// User.findAll({where:{name: "Tom"}, raw: true })
+//     .then(users=>{
+//         console.log(users);
+//     }).catch(err=>console.log(err));
+//
+// User.findByPk(2)
+//     .then(user=>{
+//         if(!user) return; // если пользователь не найден
+//         console.log(user.name);
+//     }).catch(err=>console.log(err));
+//
+// User.update({ age: 36 }, {
+//     where: {
+//         name: "Bob"
+//     }
+// }).then((res) => {
+//     console.log(res);
+// });
+//
+// User.destroy({
+//     where: {
+//         name: "Bob"
+//     }
+// }).then((res) => {
+//     console.log(res);
+// });
